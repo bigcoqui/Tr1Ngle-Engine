aa14ec07b38208a27adcbdd616ee98a646555c5c
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-11-12 11:40:57 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1111,9 +1111,9 @@ class PlayState extends MusicBeatState
 		add(iconP2);
 
 		// cool port watermark
-		var portTxt = new FlxText(0, 5, 1100);
+		var portTxt = new FlxText(0, 5, 1300);
     portTxt.text = "PORTED BY THEORDA";
-    portTxt.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+    portTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
     portTxt.scrollFactor.set();
     add(portTxt);
 
